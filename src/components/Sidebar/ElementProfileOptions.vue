dc24b2fabfd2fc78a77e1fb50ae9c21cac4aabac
JcLecha <jc.lechalier@gmail.com>
2017-06-16 20:06:35 +0200

    style(navigation): add icons for previous/next button

--- src/components/Sidebar/ElementProfileOptions.vue
+++ src/components/Sidebar/ElementProfileOptions.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="c-element-profile-options">
-    <div class="btn-group w-100" role="group" aria-label="Navigation">
-      <router-link :to="'/element/' + previousElementId" tag="button" type="button" class="btn btn-primary w-100" :disabled="previousElementId === 0">{{ $t("sidebar.elementProfile.previous") }}</router-link>
-      <router-link :to="'/element/' + nextElementId" tag="button" type="button" class="btn btn-primary w-100" :disabled="nextElementId === numberOfElements + 1">{{ $t("sidebar.elementProfile.next") }}</router-link>
-    </div>
+  <div class="btn-group w-100" role="group" aria-label="Navigation">
+    <router-link
+      :to="'/element/' + previousElementId"
+      tag="button"
+      type="button"
+      class="btn btn-primary w-100"
+      :disabled="previousElementId === 0">
+      <i class="material-icons">chevron_left</i>
+    </router-link>
+    <router-link
+      :to="'/element/' + nextElementId"
+      tag="button"
+      type="button"
+      class="btn btn-primary w-100"
+      :disabled="nextElementId === numberOfElements + 1">
+        <i class="material-icons">chevron_right</i>
+    </router-link>
+  </div>
 
     <p class="text-muted mt-3">Debug</p>
     <mdc-switch v-model="debug"></mdc-switch>
